f802c1a2f3de69a5a854b3b6f6b42a541cb629ed
Geoff Cox <redgeoff@gmail.com>
2018-12-21 09:29:59 -0800

    refactor(hello): better formatting for promises

--- src/hello.test.js
+++ src/hello.test.js
@@ -2,9 +2,6 @@ import hello from './hello';
 
 describe('hello', () => {
   it('executes as expected', async () => {
-    const cb = jest.fn();
-    await hello({}, {}, cb);
-    expect(cb).toBeCalled();
-    expect(cb).toMatchSnapshot();
+    expect(await hello({}, {})).toMatchSnapshot();
   });
 });
