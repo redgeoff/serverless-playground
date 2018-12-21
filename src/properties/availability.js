bba770315df83207011567748bdf4b1063f821bb
Geoff Cox <redgeoff@gmail.com>
2018-12-21 10:23:12 -0800

    feat(availability): yup

--- src/properties/availability.js
+++ src/properties/availability.js
@@ -1,6 +1,19 @@
 import { successResponse, runWarm, badRequestResponse } from '../utils';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  // TODO: This is just a proof of concept and not necessarily the proper format for the id
+  propertyId: yup.number().required().integer()
+});
+
+export const isValid = async (pathParameters) => schema.isValid(pathParameters)
 
 const availability = async (event, context) => {
+  if (!(await isValid(event.pathParameters))) {
+    // TODO: use yup to actually report details of why the data is invalid
+    return badRequestResponse({ errorMessage: 'yup says no' })
+  }
+
   const propertyId = event.pathParameters.propertyId;
   if (propertyId === '123') {
     return badRequestResponse({ errorMessage: 'cannot be 123' })
--- src/properties/availability.test.js
+++ src/properties/availability.test.js
@@ -1,7 +1,12 @@
-import availability from './availability';
+import availability, { isValid } from './availability';
 
 describe('availability', () => {
   it('executes as expected', async () => {
-    expect(await availability({}, {})).toMatchSnapshot();
+    expect(await availability({ pathParameters: { propertyId: '1234' } }, {})).toMatchSnapshot();
   });
+
+  it('is valid', async () => {
+    expect(await isValid({ propertyId: 'aaa' })).toEqual(false)
+    // TODO: ...
+  })
 });
