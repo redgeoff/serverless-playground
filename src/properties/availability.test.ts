2c930da23c1a33781ac0e322d5f9dd34d1d97d7f
Geoff Cox <redgeoff@gmail.com>
2019-01-28 15:26:05 -0800

    feat(json-schema)

--- src/properties/availability.test.ts
+++ src/properties/availability.test.ts
@@ -1,20 +1,27 @@
-import availability, { foo, isValid } from './availability';
+import availability, { foo, validate } from './availability';
 
-describe('availability', () => {
-  it('executes as expected', async () => {
-    expect(await availability({ pathParameters: { propertyId: '1234' } }, {})).toMatchSnapshot();
-  });
-
-  it('is valid', async () => {
-    expect(await isValid({ propertyId: 'aaa' })).toEqual(false);
-    // TODO: ...
-  });
-
-  it('should foo', () => {
-    // function yo(cool: string) {
-    //   return cool;
-    // }
-    // expect(foo());
-    expect('bar').toEqual('bar');
-  });
+// describe('availability', () => {
+it('executes as expected', async () => {
+  expect(await availability({ pathParameters: { propertyId: 1234 } }, {})).toMatchSnapshot();
 });
+
+// With yup
+// it('is valid', async () => {
+//   expect(await isValid({ propertyId: 'aaa' })).toEqual(false);
+//   // TODO: ...
+// });
+
+it('is valid', async () => {
+  const validated = await validate({ propertyId: 'aaa' })
+  expect(validated.valid).toEqual(false);
+  // TODO: ...
+});
+
+it('should foo', () => {
+  // function yo(cool: string) {
+  //   return cool;
+  // }
+  // expect(foo());
+  expect('bar').toEqual('bar');
+});
+// });
--- src/properties/availability.ts
+++ src/properties/availability.ts
@@ -1,19 +1,51 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import * as yup from 'yup';
+// import * as yup from 'yup';
 import { badRequestResponse, runWarm, successResponse } from '../utils';
+import Ajv from 'ajv';
 
-const schema = yup.object().shape({
-  // TODO: This is just a proof of concept and not necessarily the proper format for the id
-  propertyId: yup.number().required().integer(),
-});
+// TODO: move to ajv.js
+const ajv = new Ajv();
 
-export const isValid = async (pathParameters: APIGatewayProxyEvent['pathParameters']) => schema.isValid(pathParameters);
+// const schema = yup.object().shape({
+//   // TODO: This is just a proof of concept and not necessarily the proper format for the id
+//   propertyId: yup.number().required().integer(),
+// });
+
+// export const isValid = async (pathParameters: APIGatewayProxyEvent['pathParameters']) => schema.isValid(pathParameters);
+
+const schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "http://example.com/product.schema.json",
+  // "title": "Property",
+  // "description": "A property",
+  "type": "object",
+  "properties": {
+    "propertyId": {
+      // "description": "The unique identifier for a property",
+      "type": "integer"
+    }
+  },
+  "required": [ "propertyId" ]
+};
+
+// Note: use async so that can easily be swapped with yup, if we decide to change
+export const validate = async (pathParameters: APIGatewayProxyEvent['pathParameters']) => ({
+  valid: ajv.validate(schema, pathParameters),
+  errors: ajv.errors
+})
 
 const availability = async (event: APIGatewayProxyEvent /* , context */) => {
-  if (!(await isValid(event.pathParameters))) {
-    // TODO: use yup to actually report details of why the data is invalid
-    return badRequestResponse({ errorMessage: 'yup says no' });
+  const validated = await validate(event.pathParameters);
+  if (!validated.valid) {
+    // TODO: properly convert errors into errorMessage
+    return badRequestResponse({ errorMessage: JSON.stringify(validated.errors) });
   }
+
+  // With yup
+  // if (!(await isValid(event.pathParameters))) {
+  //   // TODO: use yup to actually report details of why the data is invalid
+  //   return badRequestResponse({ errorMessage: 'yup says no' });
+  // }
 
   const { propertyId= undefined } = event.pathParameters || {};
   if (propertyId === '123') {
